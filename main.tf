26d20741eac64751cf19ebc56c33431387ec2e39
Darron Froese <darron.froese@dapperlabs.com>
2021-11-29 19:04:58 -0700

    First PR.

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = ">= 1.14.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.88.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.88.0"
+    }
+  }
+}
+
+module "service_account" {
+  source       = "github.com/dapperlabs-platform/terraform-google-iam-service-account?ref=v1.0.0"
+  project_id   = var.project_name
+  name         = "${var.project_name}-metrics"
+  generate_key = true
+  iam_project_roles = {
+    "${var.project_name}" = [
+      "roles/monitoring.viewer",
+    ]
+  }
+}
+
+resource "grafana_data_source" "stackdriver" {
+  depends_on = [
+    module.service_account
+  ]
+  type = "stackdriver"
+  name = "${var.project_name}-gcp-metrics"
+
+  json_data {
+    token_uri           = "https://oauth2.googleapis.com/token"
+    authentication_type = "jwt"
+    default_project     = var.project_name
+    client_email        = module.service_account.iam_email
+  }
+
+  secure_json_data {
+    private_key = module.service_account.key.private_key
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "project_name" {
+  description = "Project name where service account will be created."
+  type        = string
+}
