30c8006163d63bf90d05564a815ddc42108db376
Marcelo Mendonça <mendonca2709@gmail.com>
2022-08-01 18:22:12 -0300

    Add project role to folder service account

--- variables.tf
+++ variables.tf
@@ -1,25 +1,21 @@
 variable "project_name" {
   description = "(Required) Project name where service account will be created."
   type        = string
-  default     = ""
 }
 
 variable "service_account_create" {
   description = "(Required) Create a new service account"
   type        = bool
-  default     = false
 }
 
 variable "grafana_datasource_name" {
   description = "(Required) Name for Grafana Datasource - only required when service_account_create is true"
   type        = string
-  default     = ""
 }
 
 variable "service_account_name" {
   description = "(Required) Name for service account"
   type        = string
-  default     = ""
 }
 
 variable "grant_folder_permissions" {
